d85caef03c94a3373ffde35fe0822c1cb4cb2cd5
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2022-11-07 15:33:07 +0100

    Add prisma & connect to ps db

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { publicProcedure, router } from "./trpc";
+import { prisma } from "../utils/prisma";
 
 export const appRouter = router({
   hello: publicProcedure
@@ -15,5 +16,4 @@ export const appRouter = router({
     }),
 });
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
